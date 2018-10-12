e395bf18cccd33e874e4e8f303a7f313cee4697d
ayuamalia1 <ayu.amalia5757@gmail.com>
2018-10-12 09:53:15 +0700

    first commit

---
+++ KaryawanTest.java
@@ -0,0 +1,19 @@
+public class KaryawanTest{
+	public static void main(String[] args) {
+		Desain d = new Desain();
+		d.setNama("Ayu amaliyah");
+		d.setAlamat("brebes");
+		d.setGaji(90);
+
+		Programmer p = new Programmer();
+		p.setNama ("Sheli");
+		p.setAlamat("Cirebon");
+		p.setGaji(80);
+
+		PM c = new PM();
+		c.setNama("Karin");
+		c.setAlamat("indramayu");
+		c.setGaji(60);
+
+	}
+}
